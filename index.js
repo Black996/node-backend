fe393e1a914f9d54235006b5cc2b9edce0b8e5cf
black996 <rharasis@yahoo.com>
2022-02-11 21:38:15 +0200

    wire up users routes

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 
 const express = require("express");
 const routes = require("./routes");
+const usersRoutes = require("./routes/users");
 
 const app = express();
 app.use(routes);
+app.use("/users", usersRoutes);
 
 app.listen(process.env.PORT, () =>
   console.log(`Running on port ${process.env.PORT}`)
